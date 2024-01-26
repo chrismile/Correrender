fa5171cac3579dc8fd931c53d45bf7e6cb7f536d
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2024-01-26 12:42:42 +0100

    Removed unused includes.

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -31,10 +31,6 @@
 #include <algorithm>
 #include <csignal>
 
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/color_space.hpp>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/rotate_vector.hpp>
 #include <boost/algorithm/string.hpp>
 
 #ifdef USE_ZEROMQ
